8d96d8be96aea77f5733d06552cf025782161b5f
Phanikumar tata <tataphani@gmail.com>
2022-02-17 11:46:48 -0600

    Update install.R

--- install.R
+++ install.R
@@ -1,5 +1,8 @@
 install.packages("tidyverse")
-install.packages("rmarkdown")
 install.packages("httr")
+install.packages("xportr")
 install.packages("shinydashboard")
 install.packages('leaflet')
+install.packages("reticulate") # python support in RMarkdown
+install.packages("ggplot2") # for plotting
+install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
